ab4e1a8b28e187d6b295898c0964fcfc88d0ebb9
unknown <AS1690@CILABS-119A-16.scilsnet.rutgers.edu>
2016-06-14 18:59:52 -0400

    Committ 5
    Game now works!

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -42,6 +42,7 @@ public class Slingshot : MonoBehaviour {
 			projectile.rigidbody.velocity = -mouseDelta * velocityMult;
 			FollowCam.S.poi = projectile;
 			projectile = null;
+			MissionDemolition.ShotFired();
 		}
 	}
 

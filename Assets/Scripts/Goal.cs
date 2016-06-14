913d92f6a756e84b08602fd2741ac8df8f33daad
TJ Smaldone <tjsmaldone@gmail.com>
2016-06-14 17:14:25 -0400

    Commit 3
    Still working on the projectile line...

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+
+	static public bool 		goalMet = false;
+	
+	void OnTriggerEnter( Collider other ) {
+		if (other.gameObject.tag == "Projectile") {
+			Goal.goalMet = true;
+			Color c = this.renderer.material.color;
+			c.g = 0;
+			c.r = 1;
+			this.renderer.material.color = c;
+		}
+	}
+}
